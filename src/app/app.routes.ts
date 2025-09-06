0bf078bf4aea6ddfc1a4fb53a811f8764b3f806a
momenelnakeeb <momenelnakeeb7@gmail.com>
2025-09-07 02:58:06 +0300

    first commit on shaghalny angular app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./home/home').then((m) => m.Home) },
+  { path: 'jobs', loadComponent: () => import('./jobs/job-list/job-list').then((m) => m.JobList) },
+  {
+    path: 'jobs/:id',
+    loadComponent: () => import('./jobs/job-card/job-card').then((m) => m.JobCard),
+  },
+  {
+    path: 'candidate/apply',
+    loadComponent: () =>
+      import('./candidate/candidate-form/candidate-form').then((m) => m.CandidateForm),
+  },
+  {
+    path: 'recruiter',
+    loadComponent: () =>
+      import('./recruiter/recruiter-dashboard/recruiter-dashboard').then(
+        (m) => m.RecruiterDashboard
+      ),
+  },
+  { path: 'login', loadComponent: () => import('./auth/login/login').then((m) => m.Login) },
+  { path: 'signup', loadComponent: () => import('./auth/signup/signup').then((m) => m.Signup) },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/jobs/job-card/job-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  imports: [],
+  templateUrl: './job-card.html',
+  styleUrl: './job-card.css'
+})
+export class JobCard {
+
+}
